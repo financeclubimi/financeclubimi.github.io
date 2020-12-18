cef007d0ca962850f2a6fef3049cf09e7d310d8c
financeclubimi <47181175+financeclubimi@users.noreply.github.com>
2020-12-19 00:26:55 +0530

    Update stockdata.js
    updated key

--- js/stockdata.js
+++ js/stockdata.js
@@ -53,7 +53,9 @@ function makeApiCall() {
     //Google sheets api
     var params = {
         //The ID of the spreadsheet to retrieve data from.
-        spreadsheetId: '11hJrOFXSRW0a7Nmfbi9yfQUfl6-kmTscyYOc-29w8gQ',
+        spreadsheetId: '1D6K_8Kzs4vJgY5LcaZEzs0uJkWKLM9ki',
+        //New Key:1D6K_8Kzs4vJgY5LcaZEzs0uJkWKLM9ki
+        //Initial key-11hJrOFXSRW0a7Nmfbi9yfQUfl6-kmTscyYOc-29w8gQ
         //The A1 notation of the values to retrieve.
         ranges: ['Stock_Prices'],
         //How values should be represented in the output.The
